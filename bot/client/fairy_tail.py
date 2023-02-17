d9d3e1dcf7f880a7fec4817558c1717fe4ac7dbd
OlejaTheHuman <newolin16@gmail.com>
2023-02-18 02:57:58 +0500

    fix

--- bot/client/fairy_tail.py
+++ bot/client/fairy_tail.py
@@ -1,6 +1,7 @@
 from aiogram import Router, types
 
 import messages as msg
+from bot.utils import get_sub_menu_folders
 from keyboards import keyboard_constructor_fairy_tail
 import buttons_texts as btn
 from magic_filter import F
@@ -21,18 +22,17 @@ async def fairy_tail_handler(
     :param callback_data: MenuCallbackFactory
     :return: None
     """
+    path = callback_data.value
+    fairy_tail_path = get_sub_menu_folders(path)
 
-    fairy_tail_number = callback_data.value
-
-    names = btn.fairy_tails[fairy_tail_number]
 
     try:
         await query.message.edit_text(text=msg.fairy_tails_message,
-                                      reply_markup=keyboard_constructor_fairy_tail(btn.fairy_tails, fairy_tail_number))
+                                      reply_markup=keyboard_constructor_fairy_tail(fairy_tail_path))
         await query.answer()
     except Exception as e:
         print(e)
         await query.message.answer(
             text=msg.fairy_tails_message,
-            reply_markup=keyboard_constructor_fairy_tail(btn.fairy_tails, fairy_tail_number)
+            reply_markup=keyboard_constructor_fairy_tail(fairy_tail_path)
         )
