2b7a3e8422e78ed996d9845e0da90e0ecd835f0a
OlejaTheHuman <newolin16@gmail.com>
2023-02-19 18:22:51 +0500

    fix

--- bot/utils.py
+++ bot/utils.py
@@ -155,9 +155,9 @@ async def get_folder_stat() -> Stat:
                     stat['sub_menu'][name] = 1
                 else:
                     stat['sub_menu'][name] += 1
-                if len(sub_menu_stat) == 0:
-                    name = get_name_from_path(decode_path(folder_name))
-                    stat['sub_menu'][name] = 0
+            if len(sub_menu_stat) == 0:
+                name = get_name_from_path(decode_path(folder_name))
+                stat['sub_menu'][name] = 0
 
         if len(folder_name) == 12:
             fairy_tail_stat = (await FairyTailStat.all().filter(path=folder_name))
@@ -167,9 +167,9 @@ async def get_folder_stat() -> Stat:
                     stat['fairy_tail'][name] = 1
                 else:
                     stat['fairy_tail'][name] += 1
-                if len(fairy_tail_stat) == 0:
-                    name = get_name_from_path(decode_path(folder_name))
-                    stat['fairy_tail'][name] = 0
+            if len(fairy_tail_stat) == 0:
+                name = get_name_from_path(decode_path(folder_name))
+                stat['fairy_tail'][name] = 0
 
     print(stat)
     return stat
