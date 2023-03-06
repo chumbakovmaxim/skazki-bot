ab296f7f6c5562a75ea985ffbe59ca4e98465039
OlejaTheHuman <newolin16@gmail.com>
2023-03-06 23:03:28 +0500

    fix

--- botsource/__init__.py
+++ botsource/__init__.py
@@ -4,10 +4,11 @@ from botsource.admin import main as admin
 from botsource import utils
 
 router = Router()
-router.include_router(start.router)
 router.include_router(menu.router)
 router.include_router(sub_menu.router)
 router.include_router(fairy_tail.router)
 router.include_router(info.router)
 router.include_router(audio.router)
 router.include_router(admin.router)
+router.include_router(start.router)
+
